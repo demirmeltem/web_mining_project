f6a4d6559db7b2cee1aa53d2772ba8d92eeee832
mersanuzun <mersanuzun@gmail.com>
2017-05-17 14:32:43 +0300

    ui fixed

--- shiny/ui.R
+++ shiny/ui.R
@@ -4,8 +4,15 @@ shinyUI(fluidPage(
   
   navbarPage("Menu",
              tabPanel("General",
-                      mainPanel(
-                        plotOutput("plot2")
+                      column(12,
+                             "Fluid 12",
+                             fluidRow(
+                               column(6,
+                                      plotOutput("pie_plot")
+                               ),
+                               column(width = 6,
+                                      "Fluid 6")
+                             )
                       )
              ),
              tabPanel("Viewer Rate",
@@ -26,7 +33,7 @@ shinyUI(fluidPage(
                         # Sidebar with a slider and selection inputs
                         sidebarPanel(
                           selectInput("selection", "Choose a series",
-                                      choices = all_series),
+                                      choices = unique(all_series$Show)),
                           actionButton("update", "Change"),
                           hr(),
                           sliderInput("freq",
